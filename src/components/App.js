c39a22b43ab22352b4b266014f8e684592e80b33
Moumita Murmu <69195751+MoumitaMurmu@users.noreply.github.com>
2023-04-30 11:38:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Tabs from './Tabs';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Tabs />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Tabs />
+    </div>
+  );
 }
 
 export default App;
